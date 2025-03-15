bd06eadf88cc39a16aa6c3aa95f6eb3965adc413
Sungjinwoo <bhushangaurav65@gmail.com>
2025-03-15 15:33:02 +0530

    Update vite.config.js

--- spotify/vite.config.js
+++ spotify/vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // If using React
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  server: {
+    proxy: mode === 'development' ? {
+      '/music': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      }
+    } : {
+      '/music': {
+        target: 'https://spotifycllone.onrender.com',
+        changeOrigin: true,
+        secure: true,
+      }
+    }
+  },
   plugins: [react()],
-  build: {
-    outDir: "dist" // Ensure the output directory is 'dist'
-  }
-});
+}));
 
