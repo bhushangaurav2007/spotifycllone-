87106f000c99db931ab67670c17a60fd554eb0c4
Sungjinwoo <bhushangaurav65@gmail.com>
2025-03-19 15:01:19 +0530

    Update App.jsx

--- spotify/src/App.jsx
+++ spotify/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
-import "./index.css";
+// import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css"; // ✅ Font Awesome for icons
 
 const API_URL = process.env.REACT_APP_API_URL || "https://spotifycllone.onrender.com"; // ✅ Dynamic API URL
